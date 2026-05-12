f3deaff716dbbf1064af6a7a7e9a0d1ff1e09d10
Harsh Durgude <harshdurgude2006@gmail.com>
2026-05-12 16:45:55 +0530

    - converted task addition to form submission for enter button also working for adding task - added task deletion logic - used label for checkbox - added few comments to improve clarity

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,26 @@ import './App.css'
 function App() {
   const [tasks, setTasks] = useState([]);
   const [input, setInput] = useState("");
-
+  function addTask() {
+    if(input.trim() !== ""){
+      setTasks([...tasks, { title: input, status: false }]);
+      // ... --> it is called spread oprator, works just as it looks
+      setInput("");
+    }
+  }
+  function deleteTask(index) {
+    setTasks(tasks.filter((task,i) => (i!==index)));
+    // react prefers creating new arrays instead of modifiying old ones for state change
+    // filter creates new array
+  }
   return (
     <>
       <div className='m-4 flex flex-col items-center'>
-
-        <div className=' *:m-2 *:border-2 *:rounded-md *:p-2'>
+        {/* made the div into form and moved add task logic to onsubmit, so that pressing enter also creates the task in todo list */}
+        <form onSubmit={(e) => {
+              e.preventDefault();
+              addTask();
+            }} className=' *:m-2 *:border-2 *:rounded-md *:p-2'>
           <input
             className=''
             type="text"
@@ -20,37 +34,39 @@ function App() {
             }} />
           <button
             className=''
-            onClick={() => {
-              if (input != "") {
-                setTasks([...tasks, { title: input, status: false }]);
-                setInput("");
-              }
-            }}
+            type='submit' //so the onSubmit code runs when this button pressed
           >Add Task</button>
-        </div>
+        </form>
 
         <div>
           {
             tasks.map((task, index) => {
-              console.log(tasks);
               return (
-                <div className='flex  gap-2 items-center'>
-                  <button className='border-2 px-0.5 m-1 rounded-sm'>Delete</button>
+                <div className='flex  gap-2 items-center' key={index}>
+                  <button 
+                    className='border-2 px-0.5 m-1 rounded-sm hover:bg-red-200' 
+                    onClick={() => deleteTask(index)}
+                    // in react event handlers we need to pass a arrow func and call our func itside it because calling 
+                    // our func directly will execute it immediately when component renders
+                  >Delete</button>
                   <p className='w-48'>{task.title}</p>
-                  <div
-                    className='p-1.5 hover:bg-gray-100'
-                    onClick={() => {
-                      const updateTasks = [...tasks];
-                      updateTasks[index].status = !updateTasks[index].status;
-                      setTasks(updateTasks);
-                    }}
+                  {/* added fixed width to move all checkboxes to the end */}
+                  <label
+                    className='p-1.5 hover:bg-gray-100 rounded-sm'
+                    // using label so that clicking on the lable also triggers click for the checkbox, because thats what lables are for
                   >
                     <input
                       className='m-1'
                       type="checkbox"
                       checked={task.status}
+                      // instead of onclick onChange is recommended for checkboxes
+                      onChange={() => {
+                        const updateTasks = [...tasks];
+                        updateTasks[index].status = !updateTasks[index].status;
+                        setTasks(updateTasks);
+                    }}
                     />
-                  </div>
+                  </label>
                 </div>
               )
             }
