1b7a572dd966db012789367d642e04d30de1733b
Harsh Durgude <harshdurgude2006@gmail.com>
2026-05-12 15:13:04 +0530

    -added tailwind -added tasklist with styles and logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,63 @@
 import { useState } from 'react'
 import './App.css'
 
+
 function App() {
   const [tasks, setTasks] = useState([]);
   const [input, setInput] = useState("");
 
   return (
     <>
-      <input type="text" 
-      value={input} 
-      onChange={(e) => {
-        setInput(e.target.value); 
-      }}/>
-      <button
-        onClick={()=>{
-          setTasks([...tasks,{title:input,status:false}])
-        }}
-      >Add Task</button>
+      <div className='m-4 flex flex-col items-center'>
 
-        {
-          tasks.map((task) =>{
-            console.log(tasks);
-            return <p>{task.title}</p>
+        <div className=' *:m-2 *:border-2 *:rounded-md *:p-2'>
+          <input
+            className=''
+            type="text"
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }} />
+          <button
+            className=''
+            onClick={() => {
+              if (input != "") {
+                setTasks([...tasks, { title: input, status: false }]);
+                setInput("");
+              }
+            }}
+          >Add Task</button>
+        </div>
+
+        <div>
+          {
+            tasks.map((task, index) => {
+              console.log(tasks);
+              return (
+                <div className='flex  gap-2 items-center'>
+                  <button className='border-2 px-0.5 m-1 rounded-sm'>Delete</button>
+                  <p className='w-48'>{task.title}</p>
+                  <div
+                    className='p-1.5 hover:bg-gray-100'
+                    onClick={() => {
+                      const updateTasks = [...tasks];
+                      updateTasks[index].status = !updateTasks[index].status;
+                      setTasks(updateTasks);
+                    }}
+                  >
+                    <input
+                      className='m-1'
+                      type="checkbox"
+                      checked={task.status}
+                    />
+                  </div>
+                </div>
+              )
+            }
+            )
           }
-          )
-        }
-      
+        </div>
+      </div>
     </>
   )
 }
